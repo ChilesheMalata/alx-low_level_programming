5b6be29df1eb79b1898a473c07a91fa8960ece56
chileshemalata <chilemdh@gmail.com>
2023-03-16 11:21:05 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - My C Programming that does a good job
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
